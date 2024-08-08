62f40d42d27e5cae43860cd6bb922cb464f434da
jilotta <tongubin@inbox.ru>
2024-08-08 19:31:21 +0300

    adding.html

--- icons/convert.sh
+++ icons/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for i in $(ls svg | cut -f1 -d'.'); do
-  inkscape svg/$i.svg -o png/$i.png -w 96 -h 96
+  inkscape svg/$i.svg -o jpg/$i.jpg -w 96 -h 96
   echo "$i.svg -> $i.png done"
 done
