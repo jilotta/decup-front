a201aa5ef0e9c97f60f3cf748f6c7f88c232e804
jilotta <tongubin@inbox.ru>
2024-08-09 06:57:00 +0300

    fixed mre

--- mre_server.py
+++ mre_server.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
+#from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 @app.route("/")
 def ping():
